b8c082eb60722270e179f8893bba0e536b749136
Honey Chauhan <hc924508@gmail.com>
2023-04-13 18:09:20 +0530

    implemented getter and setters in RWOnly

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
         RWOnly rw = new RWOnly();
 
+        //rw.name = "Honey";
+        //System.out.println(rw.name);
+        //java: name has private access in com.driver.RWOnly
+
+        rw.setName("Honey");
+        System.out.print(rw.getName());
     }
   
 }
